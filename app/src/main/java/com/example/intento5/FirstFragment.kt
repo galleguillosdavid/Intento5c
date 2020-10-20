fe6b5531d1c8cdd05090cee3796df508262160d6
David Galleguillos <galleguillos.david@gmail.comglobal user.email galleguillos.david@gmail.com>
2020-10-20 13:15:25 -0300

    1 inicio basico en 0 2 Actualizacion de bibliotecas de navegacion 3 Permisos de interne en el manifest 4 Clases importadas de json(7) 5 Super Hero Api, mas packaje pojos 6 Retrofit Client 7 Packages de network 8 Repositorio con la vieja confiable 9 Package y class View Model 10 local.room Entity 11 SuperHeroesDao.kt 12 SuperHeroesDatabase.kt 13 local.room HeightTypeConverter.kt 14 local.room SuperHeroesDatabase.kt @TypeConverters(HeightTypeConverter::class) 15 Actualizacion de SuperHeroesRepository.kt 16 Actualizacion de SuperHeroesViewModel.kt 17 Frist fragment, lanzamiento

--- app/src/main/java/com/example/intento5/FirstFragment.kt
+++ app/src/main/java/com/example/intento5/FirstFragment.kt
@@ -1,17 +1,28 @@
 package com.example.intento5
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import com.example.intento5.Model.ViewModel.SuperHeroesViewModel
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class FirstFragment : Fragment() {
+
+    lateinit var mViewModel: SuperHeroesViewModel
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mViewModel = ViewModelProvider(this).get(SuperHeroesViewModel::class.java)
+    }
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
@@ -23,6 +34,10 @@ class FirstFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        mViewModel.liveDataFromLocal.observe(viewLifecycleOwner, Observer{
+            Log.d("Fromdb", it.toString())
+        })
 
         view.findViewById<Button>(R.id.button_first).setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
