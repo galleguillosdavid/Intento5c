1be7f709e058de53c3fe1070f81231bf47c590f8
David Galleguillos <galleguillos.david@gmail.comglobal user.email galleguillos.david@gmail.com>
2020-10-20 10:42:55 -0300

    1 inicio basico en 0 2 Actualizacion de bibliotecas de navegacion 3 Permisos de interne en el manifest 4 Clases importadas de json(7) 5 Super Hero Api, mas packaje pojos 6 Retrofit Client 7 Packages de network 8 Repositorio con la vieja confiable 9 Package y class View Model 10 local.room Entity

---
+++ app/src/main/java/com/example/intento5/Model/Local/Room/SupoerHeroesEntity.kt
@@ -0,0 +1,19 @@
+package com.example.intento5.Model.Local.Room
+
+import androidx.annotation.NonNull
+import androidx.room.Embedded
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "super_heroes_table")
+data class SupoerHeroesEntity(
+    @PrimaryKey
+    @NonNull
+    val id          : Int,
+    val imageXs     : String,
+    val imageLg     : String,
+    val name        : String,
+    val AlterEgos   : String?,
+    @Embedded
+    val height      : List<String>
+)
