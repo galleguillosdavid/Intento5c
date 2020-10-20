58777975b4e314e616bd82025e2661de760f50a8
David Galleguillos <galleguillos.david@gmail.comglobal user.email galleguillos.david@gmail.com>
2020-10-20 10:29:13 -0300

    1 inicio basico en 0 2 Actualizacion de bibliotecas de navegacion 3 Permisos de interne en el manifest 4 Clases importadas de json(7) 5 Super Hero Api, mas packaje pojos 6 Retrofit Client 7 Packages de network 8 Repositorio con la vieja confiable 9 Package y class View Model

--- app/src/main/java/com/example/intento5/Model/ViewModel/SuperHeroesViewModel.kt
+++ app/src/main/java/com/example/intento5/Model/ViewModel/SuperHeroesViewModel.kt
@@ -1,2 +1,12 @@
 package com.example.intento5.Model.ViewModel
 
+import androidx.lifecycle.ViewModel
+import com.example.intento5.Model.Network.Retrofit.SuperHeroesRepository
+
+class SuperHeroesViewModel :ViewModel() {
+    private val mRepository : SuperHeroesRepository
+
+    init {
+        mRepository = SuperHeroesRepository()
+    }
+}
