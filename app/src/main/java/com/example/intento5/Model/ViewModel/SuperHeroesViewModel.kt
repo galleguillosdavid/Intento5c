5aff2169581ebb8968b36fd548e38439bcea84cc
David Galleguillos <galleguillos.david@gmail.comglobal user.email galleguillos.david@gmail.com>
2020-10-20 13:08:34 -0300

    1 inicio basico en 0 2 Actualizacion de bibliotecas de navegacion 3 Permisos de interne en el manifest 4 Clases importadas de json(7) 5 Super Hero Api, mas packaje pojos 6 Retrofit Client 7 Packages de network 8 Repositorio con la vieja confiable 9 Package y class View Model 10 local.room Entity 11 SuperHeroesDao.kt 12 SuperHeroesDatabase.kt 13 local.room HeightTypeConverter.kt 14 local.room SuperHeroesDatabase.kt @TypeConverters(HeightTypeConverter::class) 15 Actualizacion de SuperHeroesRepository.kt 16 Actualizacion de SuperHeroesViewModel.kt

--- app/src/main/java/com/example/intento5/Model/ViewModel/SuperHeroesViewModel.kt
+++ app/src/main/java/com/example/intento5/Model/ViewModel/SuperHeroesViewModel.kt
@@ -1,12 +1,23 @@
 package com.example.intento5.Model.ViewModel
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import com.example.intento5.Model.Local.Room.SuperHeroesDao
+
+import com.example.intento5.Model.Local.Room.SuperHeroesDatabase
+import com.example.intento5.Model.Local.Room.SuperHeroesEntity
 import com.example.intento5.Model.Network.Retrofit.SuperHeroesRepository
 
-class SuperHeroesViewModel :ViewModel() {
+class SuperHeroesViewModel(application: Application) :AndroidViewModel(application) {
     private val mRepository : SuperHeroesRepository
+    val liveDataFromLocal: LiveData<List<SuperHeroesEntity>>
 
     init {
-        mRepository = SuperHeroesRepository()
+        val superHeroesDao = SuperHeroesDatabase.getDatabase(application).superHeroesDao()
+        mRepository = SuperHeroesRepository(superHeroesDao)
+        mRepository.getDataFromServer()
+        liveDataFromLocal = mRepository.allSuperHeroesLiveData
     }
+
 }
